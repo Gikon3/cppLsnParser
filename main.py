6120c75df26e81260cd01d1b6fb8910e7cd3aab2
gukov <gukov@mri-progress.ru>
2021-12-30 10:39:37 +0300

    add: файлы в список обработки

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ files = ["C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\0gr_100.txt",
          "C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\60gr.txt",
          "C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\60gr1.txt",
          "C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\60gr2.txt",
-         "C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\60gr250.txt"]
+         "C:\\Users\\gukov\\Desktop\\НИИ МА НАУКА\\60gr250.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\0degree_move.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\30degree_move.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\30degree_nomove.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\60degree_move.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\60degree_nomove.txt",
+         "C:\\Users\\gukov\\Desktop\\logs_input\\lowpower(0degree).txt"]
 work_dir = "C:\\Users\\gukov\\Desktop\\workParser\\"
 
 
